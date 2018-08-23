3866d23a2e5fdeaa2dc2f6fea30577c25bb55d5e
Anthony Super <anthony@noided.media>
2018-08-22 21:15:46 -0600

    Possibly start building with nix on travis

--- default.nix
+++ default.nix
@@ -1,10 +1,11 @@
 (import ./reflex-platform {}).project ({ pkgs, ...} : {
   packages = {
     haskgames-shared = ./haskgames-shared;
+    haskgames-server = ./haskgames-server;
   };
 
   shells = {
-    ghc = ["haskgames-shared"];
+    ghc = ["haskgames-shared" "haskgames-server"];
     ghcjs = ["haskgames-shared"];
   };
 })
